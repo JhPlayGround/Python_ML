d2c6c301bb9324e5658a8fffec138c1780bcbf8d
juho1210 <wngh9512@gmail.com>
2020-06-03 21:54:19 +0900

    커널 PCA를 사용하여 비선형 매핑

---
+++ 커널 PCA를 사용하여 비선형 매핑.py
@@ -0,0 +1,205 @@
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+
+import numpy as np
+
+def rbf_kernel_pca(X, gamma, n_components):
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    mat_sq_dists = squareform(sq_dists) # 샘플 간의 거리를 정방 대칭 행렬로 변환
+    K = exp(-gamma * mat_sq_dists) # 커널 행렬을 계산
+
+    # 커널 행렬을 중앙에 맞춥니다.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # 중앙에 맞춰진 커널 행렬의 고윳값과 고유벡터를 구합니다.
+    # scipy.linalg.eigh 함수는 오름차순으로 반환합니다.
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # 최상위 k 개의 고유벡터를 선택합니다(결과값은 투영된 샘플입니다).
+    X_pc = np.column_stack([eigvecs[:, i] for i in range(n_components)])
+    return X_pc
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+
+X, y = make_moons(n_samples=100, random_state=123)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
+
+#기본 PCA의 주성분에 데이터셋 투영 
+from sklearn.decomposition import PCA
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50, 1)) - 0.02,color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#구현한 커널 PCA
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1],  color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02,  color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02,color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#동심원 분리하기
+from sklearn.datasets import make_circles
+
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#새로운 데이터 포인트 투영
+from scipy.spatial.distance import pdist, squareform
+from numpy import exp
+from scipy.linalg import eigh
+import numpy as np
+
+def rbf_kernel_pca(X, gamma, n_components):
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # 샘플 간의 거리를 정방 대칭 행렬로 변환합니다.
+    mat_sq_dists = squareform(sq_dists)
+
+    # 커널 행렬을 계산합니다.
+    K = exp(-gamma * mat_sq_dists)
+
+    # 커널 행렬을 중앙에 맞춥니다.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # 중앙에 맞춰진 커널 행렬의 고윳값과 고유 벡터를 구합니다.
+    # scipy.linalg.eigh 함수는 오름차순으로 반환합니다.
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # 최상위 k 개의 고유 벡터를 선택합니다(투영 결과).
+    alphas = np.column_stack([eigvecs[:, i]
+                              for i in range(n_components)])
+
+    # 고유 벡터에 상응하는 고윳값을 선택합니다.
+    lambdas = [eigvals[i] for i in range(n_components)]
+
+    return alphas, lambdas
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+x_new = X[25]
+print(x_new)
+
+x_proj = alphas[25] # 원본 투영
+print(x_proj)
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+# 새로운 데이터포인트를 투영
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+print(x_reproj)
+
+plt.scatter(alphas[y == 0, 0], np.zeros((50)), color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black', label='original projection of point X[25]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green', label='remapped point X[25]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+plt.tight_layout()
+plt.show()
+
+#사이킷런의 커널 PCA
+from sklearn.decomposition import KernelPCA
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.tight_layout()
+plt.show()
