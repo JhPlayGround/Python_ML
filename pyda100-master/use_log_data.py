1f762ccf49bf10ff26021e268f5cf221bd1deaa5
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-10-19 23:08:22 +0900

    use log data

--- pyda100-master/use_log_data.py
+++ pyda100-master/use_log_data.py
@@ -2,10 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
+import sklearn.model_selection
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from dateutil.relativedelta import relativedelta
+from sklearn.linear_model import LinearRegression
+
+
 
 uselog = pd.read_csv('pyda100-master/data/use_log.csv')
 print(uselog.isnull().sum())
@@ -30,7 +35,6 @@ customer_clustering.columns = ["월평균값","월중앙값", "월최댓값", "�
 print(customer_clustering.groupby("cluster").count())
 print(customer_clustering.groupby("cluster").mean())
 
-
 X = customer_clustering_sc
 pca = PCA(n_components=2)
 pca.fit(X)
@@ -38,9 +42,72 @@ x_pca = pca.transform(X)
 pca_df = pd.DataFrame(x_pca)
 pca_df["cluster"] = customer_clustering["cluster"]
 
-
-
 for i in customer_clustering["cluster"].unique():
     tmp = pca_df.loc[pca_df["cluster"]==i]
     plt.scatter(tmp[0], tmp[1])
     plt.show()
+
+customer_clustering = pd.concat([customer_clustering, customer], axis=1)
+customer_clustering.groupby(["cluster","is_deleted"],as_index=False).count()[["cluster","is_deleted","customer_id"]]
+customer_clustering.groupby(["cluster","routine_flg"],as_index=False).count()[["cluster","routine_flg","customer_id"]]
+
+uselog["usedate"] = pd.to_datetime(uselog["usedate"])
+uselog["연월"] = uselog["usedate"].dt.strftime("%Y%m")
+uselog_months = uselog.groupby(["연월","customer_id"],as_index=False).count()
+uselog_months.rename(columns={"log_id":"count"}, inplace=True)
+del uselog_months["usedate"]
+print(uselog_months.head())
+
+year_months = list(uselog_months["연월"].unique())
+predict_data = pd.DataFrame()
+for i in range(6, len(year_months)):
+    tmp = uselog_months.loc[uselog_months["연월"]==year_months[i]]
+    tmp.rename(columns={"count":"count_pred"}, inplace=True)
+    for j in range(1, 7):
+        tmp_before = uselog_months.loc[uselog_months["연월"]==year_months[i-j]]
+        del tmp_before["연월"]
+        tmp_before.rename(columns={"count":"count_{}".format(j-1)}, inplace=True)
+        tmp = pd.merge(tmp, tmp_before, on="customer_id", how="left")
+    predict_data = pd.concat([predict_data, tmp], ignore_index=True)
+
+print(predict_data.head())
+predict_data = predict_data.dropna()
+predict_data = predict_data.reset_index(drop=True)
+print(predict_data.head())
+
+predict_data = pd.merge(predict_data, customer[["customer_id","start_date"]], on="customer_id", how="left")
+print(predict_data.head())
+
+predict_data["now_date"] = pd.to_datetime(predict_data["연월"], format="%Y%m")
+predict_data["start_date"] = pd.to_datetime(predict_data["start_date"])
+predict_data["period"] = None
+
+for i in range(len(predict_data)):
+    delta = relativedelta(predict_data["now_date"][i], predict_data["start_date"][i])
+    predict_data["period"][i] = delta.years*12 + delta.months
+
+print(predict_data.head())
+
+predict_data = predict_data.loc[predict_data["start_date"]>=pd.to_datetime("20180401")]
+
+model = LinearRegression()
+X = predict_data[["count_0","count_1","count_2","count_3","count_4","count_5","period"]]
+y = predict_data["count_pred"]
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y)
+model.fit(X_train, y_train)
+
+print(model.score(X_train, y_train))
+print(model.score(X_test, y_test))
+
+coef = pd.DataFrame({"feature_names":X.columns, "coefficient":model.coef_})
+print(coef)
+
+x1 = [3, 4, 4, 6, 8, 7, 8]
+x2 = [2, 2, 3, 3, 4, 6, 8]
+x_pred = [x1, x2]
+print(x_pred)
+
+model.predict(x_pred)
+print(model.predict(x_pred))
+
+uselog_months.to_csv("pyda100-master/use_log_months.csv",index=False)
