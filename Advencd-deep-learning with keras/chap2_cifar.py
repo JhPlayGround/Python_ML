e6a850e12dfd4699b4ed9882591b24404fb346a3
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-11-15 22:58:19 +0900

    cifar1 - cnn

---
+++ Advencd-deep-learning with keras/chap2_cifar.py
@@ -0,0 +1,101 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from keras.datasets import cifar10
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+class_id = 0
+class_count = 0
+images = None
+for i in range(100):
+    while True:
+        index = np.random.randint(0, x_train.shape[0], size=1)
+        image = x_train[index]
+        if y_train[index] == class_id:
+            break
+
+    if images is None:
+        images = image
+    else:
+        images = np.concatenate([images, image], axis=0)
+    class_count += 1
+    if class_count == 10:
+        class_id += 1
+        class_count = 0
+      
+print(images.shape)
+
+plt.figure(figsize=(10, 10))
+num_images = images.shape[0]
+image_size = images.shape[1]
+rows = int(math.sqrt(num_images))
+row_names = ['{}'.format(row) for row in ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']]
+index = 0
+for i in range(num_images):
+    ax = plt.subplot(rows, rows, i + 1)
+    image = images[i, :, :, :]
+    image = np.reshape(image, [image_size, image_size, 3])
+    plt.imshow(image)
+    # plt.axis('off')
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.grid(False)
+    ax.xaxis.set_ticks_position('none') 
+    ax.yaxis.set_ticks_position('none') 
+    if (i % rows) == 0:
+        ax.set_ylabel(row_names[index], rotation=45, size='large')
+        ax.yaxis.labelpad = 20
+        print(row_names[index])
+        index += 1
+
+# plt.tight_layout()
+plt.savefig("Advencd-deep-learning with keras/cifar10-samples.png")
+plt.show()
+plt.close('all')
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+num_labels = len(np.unique(y_train))
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train,[-1, image_size, image_size, 1])
+x_test = np.reshape(x_test,[-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+input_shape = (image_size, image_size, 1)
+batch_size = 128
+kernel_size = 3
+filters = 64
+dropout = 0.3
+
+inputs = Input(shape=input_shape)
+y = Conv2D(filters=filters, kernel_size=kernel_size, activation='relu')(inputs)
+y = MaxPooling2D()(y)
+y = Conv2D(filters=filters, kernel_size=kernel_size, activation='relu')(y)
+y = MaxPooling2D()(y)
+y = Conv2D(filters=filters, kernel_size=kernel_size, activation='relu')(y)
+y = Flatten()(y)
+y = Dropout(dropout)(y)
+outputs = Dense(num_labels, activation='softmax')(y)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=batch_size)
+
+score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+print("\n Test accuracy: %.1f%%" % (100.0 * score[1]))
