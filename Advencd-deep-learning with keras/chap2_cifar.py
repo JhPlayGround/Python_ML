60a59cdf4743c5c405da1c73dd4c9753fe2d94d8
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-11-17 23:00:32 +0900

    cnn

--- Advencd-deep-learning with keras/chap2_cifar.py
+++ Advencd-deep-learning with keras/chap2_cifar.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 
 from keras.datasets import cifar10
 from tensorflow.keras.layers import Dense, Dropout, Input
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, concatenate
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical, plot_model
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -98,4 +98,64 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=batch_size)
 
 score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+print("\n Test accuracy: %.1f%%" % (100.0 * score[1]))
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+num_labels = len(np.unique(y_train))
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train,[-1, image_size, image_size, 1])
+x_test = np.reshape(x_test,[-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+input_shape = (image_size, image_size, 1)
+batch_size = 32
+kernel_size = 3
+dropout = 0.4
+n_filters = 32
+
+left_inputs = Input(shape=input_shape)
+x = left_inputs
+filters = n_filters
+
+# 3 layers of Conv2D-Dropout-MaxPooling2D
+# number of filters doubles after each layer (32-64-128)
+for i in range(3):
+    x = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='relu')(x)
+    x = Dropout(dropout)(x)
+    x = MaxPooling2D()(x)
+    filters *= 2
+
+right_inputs = Input(shape=input_shape)
+y = right_inputs
+filters = n_filters
+
+for i in range(3):
+    y = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='relu', dilation_rate=2)(y)
+    y = Dropout(dropout)(y)
+    y = MaxPooling2D()(y)
+    filters *= 2
+
+y = concatenate([x, y])
+
+y = Flatten()(y)
+y = Dropout(dropout)(y)
+outputs = Dense(num_labels, activation='softmax')(y)
+
+model = Model([left_inputs, right_inputs], outputs)
+
+#plot_model(model, to_file='cnn-y-network.png', show_shapes=True)
+
+print(model.summary())
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit([x_train, x_train], y_train,  validation_data=([x_test, x_test], y_test), epochs=20, batch_size=batch_size)
+
+score = model.evaluate([x_test, x_test], y_test, batch_size=batch_size, verbose=0)
+
 print("\n Test accuracy: %.1f%%" % (100.0 * score[1]))
