bfcc63a23c0d9bc2908f3187c46b8ac2ffbfaa01
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-11-23 22:58:53 +0900

    densenet

--- Advencd-deep-learning with keras/chap2_cifar.py
+++ Advencd-deep-learning with keras/chap2_cifar.py
@@ -14,7 +14,7 @@ from tensorflow.keras.layers import AveragePooling2D, add
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical, plot_model
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -353,5 +353,153 @@ else:
               validation_data=(x_test, y_test), steps_per_epoch=steps_per_epoch, callbacks=callbacks)
 
 scores = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
+
+#densenet
+
+
+# training parameters
+batch_size = 32
+epochs = 200
+data_augmentation = True
+
+# network parameters
+num_classes = 10
+num_dense_blocks = 3
+use_max_pool = False
+
+growth_rate = 12
+depth = 100
+num_bottleneck_layers = (depth - 4) // (2 * num_dense_blocks)
+num_filters_bef_dense_block = 2 * growth_rate
+compression_factor = 0.5
+
+# load the CIFAR10 data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+input_shape = x_train.shape[1:]
+
+# normalize data
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+# convert class vectors to binary class matrices.
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+def lr_schedule(epoch):
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+# densenet CNNs (composite function) are made of BN-ReLU-Conv2D
+inputs = Input(shape=input_shape)
+x = BatchNormalization()(inputs)
+x = Activation('relu')(x)
+x = Conv2D(num_filters_bef_dense_block,
+           kernel_size=3,
+           padding='same',
+           kernel_initializer='he_normal')(x)
+x = concatenate([inputs, x])
+
+# stack of dense blocks bridged by transition layers
+for i in range(num_dense_blocks):
+    for j in range(num_bottleneck_layers):
+        y = BatchNormalization()(x)
+        y = Activation('relu')(y)
+        y = Conv2D(4 * growth_rate, kernel_size=1, padding='same', kernel_initializer='he_normal')(y)
+
+        if not data_augmentation:
+            y = Dropout(0.2)(y)
+        y = BatchNormalization()(y)
+        y = Activation('relu')(y)
+        y = Conv2D(growth_rate, kernel_size=3, padding='same', kernel_initializer='he_normal')(y)
+
+        if not data_augmentation:
+            y = Dropout(0.2)(y)
+
+        x = concatenate([x, y])
+
+    if i == num_dense_blocks - 1:
+        continue
+
+    num_filters_bef_dense_block += num_bottleneck_layers * growth_rate
+    num_filters_bef_dense_block = int(num_filters_bef_dense_block * compression_factor)
+
+    y = BatchNormalization()(x)
+    y = Conv2D(num_filters_bef_dense_block, kernel_size=1, padding='same', kernel_initializer='he_normal')(y)
+
+    if not data_augmentation:
+        y = Dropout(0.2)(y)
+
+    x = AveragePooling2D()(y)
+
+x = AveragePooling2D(pool_size=8)(x)
+y = Flatten()(x)
+outputs = Dense(num_classes, kernel_initializer='he_normal', activation='softmax')(y)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(1e-3), metrics=['acc'])
+model.summary()
+
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'cifar10_densenet_model.{epoch:02d}.h5'
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+
+checkpoint = ModelCheckpoint(filepath=filepath, monitor='val_acc', verbose=1, save_best_only=True)
+lr_scheduler = LearningRateScheduler(lr_schedule)
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
+
+callbacks = [checkpoint, lr_reducer, lr_scheduler]
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              callbacks=callbacks)
+else:
+    datagen = ImageDataGenerator(
+        featurewise_center=False, 
+        samplewise_center=False, 
+        featurewise_std_normalization=False, 
+        samplewise_std_normalization=False,  
+        zca_whitening=False, 
+        rotation_range=0, 
+        width_shift_range=0.1,
+        height_shift_range=0.1,  
+        horizontal_flip=True,  
+        vertical_flip=False)
+
+    datagen.fit(x_train)
+
+    steps_per_epoch = math.ceil(len(x_train) / batch_size)
+
+    model.fit(x=datagen.flow(x_train, y_train, batch_size=batch_size),
+              verbose=1,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              steps_per_epoch=steps_per_epoch,
+              callbacks=callbacks)
+
+scores = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
