dec3f4595b5a5c8b76a74619b2ca312aa9b81eab
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-12-06 22:50:42 +0900

    autoencoder - 2dimension mnist

--- Advencd-deep-learning with keras/chap3_autoencoders.py
+++ Advencd-deep-learning with keras/chap3_autoencoders.py
@@ -12,6 +12,7 @@ from tensorflow.keras import backend as K
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 (x_train, _), (x_test, _) = mnist.load_data()
 
@@ -76,3 +77,124 @@ plt.imshow(imgs, interpolation='none', cmap='gray')
 plt.savefig('input_and_decoded.png')
 plt.show()
 
+#autoencoder - 2dimesion mnist
+def plot_results(models, data, batch_size=32, model_name="autoencoder_2dim"):
+    encoder, decoder = models
+    x_test, y_test = data
+    xmin = ymin = -4
+    xmax = ymax = +4
+    os.makedirs(model_name, exist_ok=True)
+
+    filename = os.path.join(model_name, "latent_2dim.png")
+    z = encoder.predict(x_test, batch_size=batch_size)
+    plt.figure(figsize=(12, 10))
+
+    axes = plt.gca()
+    axes.set_xlim([xmin,xmax])
+    axes.set_ylim([ymin,ymax])
+
+    z = z[0::2]
+    y_test = y_test[0::2]
+    plt.scatter(z[:, 0], z[:, 1], marker="")
+    for i, digit in enumerate(y_test):
+        axes.annotate(digit, (z[i, 0], z[i, 1]))
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.savefig(filename)
+    plt.show()
+
+    filename = os.path.join(model_name, "digits_over_latent.png")
+    n = 30
+    digit_size = 28
+    figure = np.zeros((digit_size * n, digit_size * n))
+
+    grid_x = np.linspace(xmin, xmax, n)
+    grid_y = np.linspace(ymin, ymax, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z = np.array([[xi, yi]])
+            x_decoded = decoder.predict(z)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+
+    plt.figure(figsize=(10, 10))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range + 1
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.imshow(figure, cmap='Greys_r')
+    plt.savefig(filename)
+    plt.show()
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+input_shape = (image_size, image_size, 1)
+batch_size = 32
+kernel_size = 3
+latent_dim = 2
+layer_filters = [32, 64]
+
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+
+for filters in layer_filters:
+    x = Conv2D(filters=filters, kernel_size=kernel_size, activation='relu', strides=2, padding='same')(x)
+
+shape = K.int_shape(x)
+
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+encoder = Model(inputs, latent, name='encoder')
+print(encoder.summary())
+plot_model(encoder, to_file='encoder.png', show_shapes=True)
+
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters, kernel_size=kernel_size, activation='relu', strides=2, padding='same')(x)
+
+outputs = Conv2DTranspose(filters=1, kernel_size=kernel_size, activation='sigmoid', padding='same', name='decoder_output')(x)
+
+decoder = Model(latent_inputs, outputs, name='decoder')
+print(decoder.summary())
+plot_model(decoder, to_file='decoder.png', show_shapes=True)
+
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+print(autoencoder.summary())
+plot_model(autoencoder, to_file='autoencoder.png', show_shapes=True)
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+autoencoder.fit(x_train, x_train, validation_data=(x_test, x_test), epochs=20, batch_size=batch_size)
+
+x_decoded = autoencoder.predict(x_test)
+
+imgs = np.concatenate([x_test[:8], x_decoded[:8]])
+imgs = imgs.reshape((4, 4, image_size, image_size))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+plt.figure()
+plt.axis('off')
+plt.title('Input: 1st 2 rows, Decoded: last 2 rows')
+plt.imshow(imgs, interpolation='none', cmap='gray')
+plt.savefig('input_and_decoded.png')
+plt.show()
+
+models = (encoder, decoder)
+data = (x_test, y_test)
+plot_results(models, data, batch_size=batch_size, model_name="autoencoder-2dim")
