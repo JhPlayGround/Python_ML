08015b16dbef08bb1555943e5a22ba4d10d9908c
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-12-09 23:29:56 +0900

    denoising autoencoder mnist

--- Advencd-deep-learning with keras/chap3_autoencoders.py
+++ Advencd-deep-learning with keras/chap3_autoencoders.py
@@ -19,10 +19,11 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
-
 import math
-import matplotlib.pyplot as plt
+from PIL import Image
+
+np.random.seed(1337)
+
 (x_train, _), (x_test, _) = mnist.load_data()
 
 image_size = x_train.shape[1]
@@ -304,3 +305,82 @@ plt.show()
 
 latent = encoder.predict(x_test)
 print("Variance:", K.var(latent))
+
+
+#denoising autoencoder mnist
+(x_train, _), (x_test, _) = mnist.load_data()
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_train.shape)
+x_train_noisy = x_train + noise
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
+x_test_noisy = x_test + noise
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+input_shape = (image_size, image_size, 1)
+batch_size = 32
+kernel_size = 3
+latent_dim = 16
+
+layer_filters = [32, 64]
+
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+
+for filters in layer_filters:
+    x = Conv2D(filters=filters, kernel_size=kernel_size, strides=2, activation='relu', padding='same')(x)
+
+shape = K.int_shape(x)
+
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+encoder = Model(inputs, latent, name='encoder')
+print(encoder.summary())
+
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+
+x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=2, activation='relu', padding='same')(x)
+
+outputs = Conv2DTranspose(filters=1, kernel_size=kernel_size, padding='same', activation='sigmoid', name='decoder_output')(x)
+
+decoder = Model(latent_inputs, outputs, name='decoder')
+print(decoder.summary())
+
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+print(autoencoder.summary())
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+autoencoder.fit(x_train_noisy, x_train, validation_data=(x_test_noisy, x_test), epochs=10, batch_size=batch_size)
+
+x_decoded = autoencoder.predict(x_test_noisy)
+
+rows, cols = 3, 9
+num = rows * cols
+imgs = np.concatenate([x_test[:num], x_test_noisy[:num], x_decoded[:num]])
+imgs = imgs.reshape((rows * 3, cols, image_size, image_size))
+imgs = np.vstack(np.split(imgs, rows, axis=1))
+imgs = imgs.reshape((rows * 3, -1, image_size, image_size))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+imgs = (imgs * 255).astype(np.uint8)
+plt.figure()
+plt.axis('off')
+plt.title('Original images: top rows, '
+          'Corrupted Input: middle rows, '
+          'Denoised Input:  third rows')
+plt.imshow(imgs, interpolation='none', cmap='gray')
+Image.fromarray(imgs).save('corrupted_and_denoised.png')
+plt.show()
