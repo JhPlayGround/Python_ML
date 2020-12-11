e02985575179f39cd00631b3bbd74a1a54cb1448
LAPTOP-JA072445\wngh9 <wngh9512@gmail.com>
2020-12-12 00:21:09 +0900

    colorization autoencoder(cifar10)

--- Advencd-deep-learning with keras/chap3_autoencoders.py
+++ Advencd-deep-learning with keras/chap3_autoencoders.py
@@ -9,10 +9,10 @@ from tensorflow.keras.layers import Reshape, Conv2DTranspose
 from tensorflow.keras.layers import Activation, Dropout, BatchNormalization
 from tensorflow.keras.layers import MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Model
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist, cifar10
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint
 from tensorflow.keras import backend as K
 
 import keras
@@ -383,4 +383,120 @@ plt.title('Original images: top rows, '
           'Denoised Input:  third rows')
 plt.imshow(imgs, interpolation='none', cmap='gray')
 Image.fromarray(imgs).save('corrupted_and_denoised.png')
+plt.show()
+
+
+#colorization-autoencoder(cifar data)
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+(x_train, _), (x_test, _) = cifar10.load_data()
+
+img_rows = x_train.shape[1]
+img_cols = x_train.shape[2]
+channels = x_train.shape[3]
+
+imgs_dir = 'saved_images'
+save_dir = os.path.join(os.getcwd(), imgs_dir)
+if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+imgs = x_test[:100]
+imgs = imgs.reshape((10, 10, img_rows, img_cols, channels))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+plt.figure()
+plt.axis('off')
+plt.title('Test color images (Ground  Truth)')
+plt.imshow(imgs, interpolation='none')
+plt.savefig('%s/test_color.png' % imgs_dir)
+plt.show()
+
+x_train_gray = rgb2gray(x_train)
+x_test_gray = rgb2gray(x_test)
+
+imgs = x_test_gray[:100]
+imgs = imgs.reshape((10, 10, img_rows, img_cols))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+plt.figure()
+plt.axis('off')
+plt.title('Test gray images (Input)')
+plt.imshow(imgs, interpolation='none', cmap='gray')
+plt.savefig('%s/test_gray.png' % imgs_dir)
+plt.show()
+
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+x_train_gray = x_train_gray.astype('float32') / 255
+x_test_gray = x_test_gray.astype('float32') / 255
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
+
+x_train_gray = x_train_gray.reshape(x_train_gray.shape[0], img_rows, img_cols, 1)
+x_test_gray = x_test_gray.reshape(x_test_gray.shape[0], img_rows, img_cols, 1)
+
+input_shape = (img_rows, img_cols, 1)
+batch_size = 32
+kernel_size = 3
+latent_dim = 256
+
+layer_filters = [64, 128, 256]
+
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+
+for filters in layer_filters:
+    x = Conv2D(filters=filters, kernel_size=kernel_size, strides=2, activation='relu', padding='same')(x)
+
+shape = K.int_shape(x)
+
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+encoder = Model(inputs, latent, name='encoder')
+print(encoder.summary())
+
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+x = Dense(shape[1]*shape[2]*shape[3])(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=2, activation='relu', padding='same')(x)
+
+outputs = Conv2DTranspose(filters=channels, kernel_size=kernel_size, activation='sigmoid', padding='same', name='decoder_output')(x)
+
+decoder = Model(latent_inputs, outputs, name='decoder')
+print(decoder.summary())
+
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+print(autoencoder.summary())
+
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'colorized_ae_model.{epoch:03d}.h5'
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, verbose=1, min_lr=0.5e-6)
+
+checkpoint = ModelCheckpoint(filepath=filepath, monitor='val_loss', verbose=1, save_best_only=True)
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+callbacks = [lr_reducer, checkpoint]
+
+autoencoder.fit(x_train_gray, x_train, validation_data=(x_test_gray, x_test), epochs=30, batch_size=batch_size, callbacks=callbacks)
+
+x_decoded = autoencoder.predict(x_test_gray)
+
+imgs = x_decoded[:100]
+imgs = imgs.reshape((10, 10, img_rows, img_cols, channels))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+plt.figure()
+plt.axis('off')
+plt.title('Colorized test images (Predicted)')
+plt.imshow(imgs, interpolation='none')
+plt.savefig('%s/colorized.png' % imgs_dir)
 plt.show()
